15e2269d9ba2d50238abc638a2f1f23202303bd1
kutay-celebi <celebikutay@gmail.com>
2020-06-19 01:08:38 +0300

    - backdrop was added for each api call.

--- ui/src/app/service/apiClient.js
+++ ui/src/app/service/apiClient.js
@@ -4,39 +4,42 @@ import axios from "axios";
 import {logout} from "../redux/actions/auth";
 import history from "../../@history";
 import {AUTH_PATH, BASE_PATH, PATH_LOGIN_ROOT} from "../constants";
+import {enqueueSnackbar} from "../redux/actions/core";
+import {closeBackdrop, openBackdrop} from "../redux/actions/core/common.actions";
 
-
-/** Intercept any unauthorized request.
- * dispatch logout action accordingly **/
-
-const apiClient = axios.create({
+export const apiClient = axios.create({
                                    baseURL: BASE_PATH + AUTH_PATH,
                                    headers: {
                                        "content-type": "application/json",
                                        "Accept"      : "application/json"
                                    },
-                                   timeout: 5000
+                                   timeout: 60000
                                });
 
 const {dispatch} = store; // direct access to redux store.
 apiClient.interceptors.request.use((config) => {
+    dispatch(openBackdrop());
     config.headers.Authorization = "Bearer " + localStorage.getItem("access_token"); //todo constantlara alalim alternatif cozum bulalim
     return config;
 });
 
 apiClient.interceptors.response.use(
-    response => response,
+    response => {
+        dispatch(closeBackdrop());
+        return response;
+    },
     error => {
-
+        dispatch(closeBackdrop());
         if (error.response.status === 401) {
             dispatch(logout());
             localStorage.removeItem("access_token");
             history.push({
                              pathname: PATH_LOGIN_ROOT
                          });
+        } else {
+            dispatch(enqueueSnackbar(error.response.data.message, {variant: "error"})); //todo generic error method.
+            return Promise.reject(error)
         }
         return error;
     }
 );
-
-export default apiClient;
