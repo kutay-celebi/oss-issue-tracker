f13b0db151beb236ddbdc4b0ade1c8ec1d5857ad
Kutay Çelebi <celebikutay@gmail.com>
2020-08-10 00:29:12 +0300

    - kuartz-70 add new components

---
+++ vue/components/form/PageContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="formHeader">
+      Header
+    </div>
+    <slot name="content"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CrudPage"
+  }
+</script>
+
+<style scoped>
+  .formHeader {
+    margin: 1rem 0
+  }
+</style>
